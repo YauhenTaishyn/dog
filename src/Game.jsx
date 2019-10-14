55b07cc23721e23c79d534341156e881d444f834
YauhenTaishyn <yauhen1990@gmail.com>
2019-10-14 16:32:03 +0300

    dog

---
+++ src/Game.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from 'src/Game.module.css'
+import './App.css';
+
+let  Game =()=> {
+    return  <div className={s.game}>hello</div>
+
+};
+
+
+export default Game;
